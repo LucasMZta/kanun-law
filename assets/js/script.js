1dac540974a9778088856b92073a8510e44244d2
Lucas Martins <lucas2zonta@hotmail.com>
2022-10-19 17:16:58 -0300

    Finalizado a area do carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,25 @@ window.onscroll = function(){
 }
 let navbar = document.getElementById('navbar');
 let carousel = document.getElementById('slideShow');
+//let carousel = window.getComputedStyle(document.getElementById('slideShow'));
+//let nav = window.getComputedStyle(document.getElementById('navbar'));
+let car = window.getComputedStyle(carousel);
+// let margin = mTop.height;
+// let mTop = document.getElementById('navbar').style.marginTop.value;
+// console.log(margin);
+
+
+
+
 let sticky = navbar.offsetTop;
 
 function fixNavbar() {
     if (window.pageYOffset >= sticky) {
+        console.log(sticky);
         navbar.classList.add("sticky");
         carousel.style.marginTop = '72px';
+        console.log(car.marginTop);
+        
     } else {
         navbar.classList.remove("sticky");
         carousel.style.marginTop = '0px';
